b4b221a2df5f6a303ebed9da61612eb3e118b67b
Ben Hall <ben.hall@intelliware.com>
2017-12-11 20:12:02 -0500

    Reformat and rename for PEP-8

--- reactive_dates_test.py
+++ reactive_dates_test.py
@@ -9,18 +9,18 @@ class TestDateFunction(unittest.TestCase):
         r = (d(2012, 1, 1), d(2013, 1, 1))
         actual = list(reactive_dates.monthly(15, r))
         expected = [
-            d(2012, 1,15),
-            d(2012, 2,15),
-            d(2012, 3,15),
-            d(2012, 4,15),
-            d(2012, 5,15),
-            d(2012, 6,15),
-            d(2012, 7,15),
-            d(2012, 8,15),
-            d(2012, 9,15),
-            d(2012,10,15),
-            d(2012,11,15),
-            d(2012,12,15)]
+            d(2012, 1, 15),
+            d(2012, 2, 15),
+            d(2012, 3, 15),
+            d(2012, 4, 15),
+            d(2012, 5, 15),
+            d(2012, 6, 15),
+            d(2012, 7, 15),
+            d(2012, 8, 15),
+            d(2012, 9, 15),
+            d(2012, 10, 15),
+            d(2012, 11, 15),
+            d(2012, 12, 15)]
         self.assertEqual(actual, expected)
 
     def test_daily(self):
@@ -29,7 +29,7 @@ class TestDateFunction(unittest.TestCase):
         expected = [
             d(2012, 1, 1),
             d(2012, 1, 2),
-            d(2012, 1, 3),]
+            d(2012, 1, 3)]
         self.assertEqual(actual, expected)
 
     def test_weekly(self):
@@ -46,22 +46,21 @@ class TestDateFunction(unittest.TestCase):
 
     def test_or(self):
         r = (d(2011, 12, 31), d(2012, 1, 14))
-        actual_orL = reactive_dates.orL(
+        actual_or_p = reactive_dates.orL(
             reactive_dates.weeklyL(d(2012, 1, 1)),
             reactive_dates.weeklyL(d(2012, 1, 3)))
-        actual = list(filter(actual_orL, reactive_dates.daily(r)))
+        actual = list(filter(actual_or_p, reactive_dates.daily(r)))
         expected = [
             d(2012, 1, 1),
             d(2012, 1, 3),
             d(2012, 1, 8),
-            d(2012, 1, 10),
-        ]
+            d(2012, 1, 10)]
         self.assertEqual(actual, expected)
 
     def test_on(self):
         r = (d(2011, 12, 31), d(2012, 1, 14))
-        actual_onL = reactive_dates.onL(d(2012, 1, 1))
-        actual = list(filter(actual_onL, reactive_dates.daily(r)))
+        actual_on_p = reactive_dates.onL(d(2012, 1, 1))
+        actual = list(filter(actual_on_p, reactive_dates.daily(r)))
         expected = [d(2012, 1, 1)]
         self.assertEqual(actual, expected)
 
